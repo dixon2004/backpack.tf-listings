323e16d05c309a988e29c427fac91633e8827729
Dixon <lokedixon@gmail.com>
2024-10-14 19:59:27 +0800

    Ensure continuous background refresh of listings

--- src/utils/tasks.py
+++ src/utils/tasks.py
@@ -16,19 +16,21 @@ class BackgroundTasks:
         """
         Refresh listings in the database.
         """
-        try:
-            collections = await self.listings_db.get_collections()
-            random.shuffle(collections)
-            for sku in collections:
-                try:
-                    listings = await self.bptf.get_listings(sku)
-                    if not listings:
-                        raise Exception("Listings not found.")
-                    
-                    item_name = listings[0]["name"]
-                    write_log("info", f"[BackgroundTasks] Refreshed listings: {item_name}")
-                except Exception as e:
-                    write_log("error", f"[BackgroundTasks] Failed to refresh listings ({sku}): {e}")
-                await asyncio.sleep(1)
-        except Exception as e:
-            write_log("error", f"[BackgroundTasks] Failed to refresh listings in background: {e}")
+        while True:
+            try:
+                collections = await self.listings_db.get_collections()
+                random.shuffle(collections)
+                for sku in collections:
+                    try:
+                        listings = await self.bptf.get_listings(sku)
+                        if not listings:
+                            raise Exception("Listings not found.")
+                        
+                        item_name = listings[0]["name"]
+                        write_log("info", f"[BackgroundTasks] Refreshed listings: {item_name}")
+                    except Exception as e:
+                        write_log("error", f"[BackgroundTasks] Failed to refresh listings ({sku}): {e}")
+                    await asyncio.sleep(1)
+            except Exception as e:
+                write_log("error", f"[BackgroundTasks] Failed to refresh listings in background: {e}")
+            await asyncio.sleep(60)
